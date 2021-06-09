6e289867e9ff4af0621d94bb99a20777bbb2b0b4
RavliukSerhii <ravliuk.serhii@chnu.edu.ua>
2021-06-09 22:42:29 +0300

    micropost.rb, user.rb and rubocop

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,9 @@
 class Micropost < ApplicationRecord
+  belongs_to :user
+
+  validates :content, length: {
+    minimum: 3,
+    maximum: 140
+  }
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2021_06_02_222634) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
